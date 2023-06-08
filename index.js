707e78b74434c63a2e6fd5a80f6fb465194e2c03
Rodrigo Thauby <rthauby@Rodrigos-MacBook-Pro.local>
2023-06-08 10:44:23 -0400

    fix port num for heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import getEntries from './queries.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5001
 
 app.use(bodyParser.json())
 app.use(
